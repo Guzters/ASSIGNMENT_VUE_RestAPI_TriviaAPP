e109bd8746cd26f2dac5b621504f278d7078ceb9
Guzters <GaJR.B@outlook.com>
2023-03-09 10:30:21 -0600

    Added 2 new files and distributed sections of the "App.vue" into those 2 new files to make it prettier

---
+++ src/components/MainLayout.vue
@@ -0,0 +1,22 @@
+<script setup>
+import MainScore from "@/components/MainScore.vue"
+</script>
+
+<template>
+  <main>
+    <div class="card">
+      <RouterView />
+    </div>
+  </main>
+  <MainScore />
+</template>
+
+<style lang="postcss" scoped>
+main{
+  @apply justify-center items-center h-screen bg-gradient-to-t from-green-200 via-green-400 to-purple-700;
+  & .card {
+    @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-white p-8 shadow-lg;
+  }
+}
+
+</style>
