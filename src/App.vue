0df49724d25c72119cca13c728e938db5874785a
Guzters <GaJR.B@outlook.com>
2023-03-08 22:46:22 -0600

    Added categories for the trivia and changed layout of the mainpage

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  <RouterView />
+  <main>
+    <div class="card">
+      <RouterView />
+    </div>
+  </main>
 </template>
+
+<style lang="postcss" scoped>
+main{
+  @apply justify-center items-center h-screen bg-gradient-to-t from-green-200 via-green-400 to-purple-700;
+  & .card {
+    @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-white p-8 shadow-lg;
+  }
+}
+</style>
