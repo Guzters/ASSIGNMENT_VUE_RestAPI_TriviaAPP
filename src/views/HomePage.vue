a32b344d8e7688cb3706b22962800d30cc08ec79
Guzters <GaJR.B@outlook.com>
2023-03-09 11:30:51 -0600

    Added new file and moved things around from HomePage.vue to make it prettier

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { onMounted, ref } from 'vue'
 import useAPI from '@/composables/useAPI'
+import BaseTitle from "@/components/BaseTitle.vue"
 
 const api = useAPI()
 const categories = ref([])
@@ -11,35 +12,25 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="brand">
-    <img class="logo" src="logo.svg" alt="logo" />
-    <h1 class="title">Triviatastic</h1>
-    <img class="logo" src="logo.svg" alt="logo" />
-  </div>
+  <BaseTitle>
+    <template #logo>
+      <img class="logo" src="logo.svg" alt="logo" />
+    </template>
+    Triviatastic
+  </BaseTitle>
   <div class="categories">
-    <RouterLink :to="`/question/category/${category.id}`" v-for="category in categories" :key="category.id" class="category">{{ category.name }}</RouterLink>
+    <RouterLink :to="`/question/category/${category.id}`" v-for="category in categories" 
+    :key="category.id" 
+    class="category">{{ category.name }}</RouterLink>
   </div>
 </template>
 
 <style lang="postcss" scoped>
-  .brand{
-    @apply flex items-center justify-center gap-4;
-
-    & .logo {
-      @apply h-16 w-16;
-    }
-
-    & .title{
-      @apply text-6xl font-thin uppercase tracking-widest text-gray-600;
-    }
-  }
-
-  .categories {
+.categories {
   @apply grid flex-grow grid-cols-4 gap-12;
 
-    & .category{
+  .category{
       @apply flex h-32 items-center justify-center rounded-lg border-4 border-green-500 py-4 text-center font-bold uppercase text-slate-600 transition-colors duration-300;
-    
 
     &:hover{
       @apply cursor-pointer border-red-500 bg-red-500 text-white;
