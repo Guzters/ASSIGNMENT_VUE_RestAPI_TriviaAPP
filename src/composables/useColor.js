e35c3f3bb8e50995de56cab33c88ae36fb7a735b
Guzters <GaJR.B@outlook.com>
2023-03-09 22:14:49 -0600

    Commented out the first iteration of the color and instead just used one, since it seems to color code "green" the right answer

--- src/composables/useColor.js
+++ src/composables/useColor.js
@@ -1,7 +1,8 @@
 import shuffle from 'lodash-es'
 
 const useColor = () => {
-    const colors = ['bg-green-700', 'bg-red-700', 'bg-blue-700', 'bg-yellow-700']
+    //const colors = ['bg-green-700', 'bg-red-700', 'bg-blue-700', 'bg-yellow-700']
+    const colors = ['bg-blue-700']
     const getColor = (index) => colors[index % colors.length]
     return { getColor }
 }
